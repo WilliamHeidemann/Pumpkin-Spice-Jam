2b71578b08ce6e1e3f83e1c2888ea4ec98c8e853
William Skou Heidemann <williamskou2227@gmail.com>
2025-11-08 18:15:36 +0100

    camera controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _camera;
+    [SerializeField] private float _speed;
+
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        MoveCamera(horizontal, vertical);
+    }
+
+    private void MoveCamera(float horizontal, float vertical)
+    {
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+        
+        Vector3 moveDir = forward * vertical + right * horizontal;
+        transform.position += moveDir * _speed * Time.deltaTime;
+
+    }
+}
